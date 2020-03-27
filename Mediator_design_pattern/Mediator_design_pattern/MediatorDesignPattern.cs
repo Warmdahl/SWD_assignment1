c97d5f9c231064593e2651a1e1e236dc66d1a713
Maja <maja.sitanala@gmail.com>
2020-03-27 09:06:55 +0100

    incommented

--- Mediator_design_pattern/Mediator_design_pattern/MediatorDesignPattern.cs
+++ Mediator_design_pattern/Mediator_design_pattern/MediatorDesignPattern.cs
@@ -10,15 +10,15 @@ namespace Mediator_design_pattern
             Airplane sparrow101 = new Airplane(controlTowerMediator);
             Runway mainRunway = new Runway(controlTowerMediator);
 
-            sparrow101.Land();
+            //sparrow101.Land();
 
             controlTowerMediator.RegisterAirVehicle(sparrow101);
             controlTowerMediator.RegisterRunway(mainRunway);
 
-            //sparrow101.Land();
-            //System.Threading.Thread.Sleep(1000);
-            //sparrow101.Land();
-            //sparrow101.TakeOf();
+            sparrow101.Land();
+            System.Threading.Thread.Sleep(1000);
+            sparrow101.Land();
+            sparrow101.TakeOf();
 
         }
     }
